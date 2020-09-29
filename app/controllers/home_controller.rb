c275ee1bd1b5f50e59af2ddb1c93a71540dd7874
I-TatsuyaEP <c1000724@planet.kanazawa-it.ac.jp>
2020-09-29 18:49:28 +0900

    完成しなかった

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,4 @@
 class HomeController < ApplicationController
-  before_action :configure_permitted_parameters
   def index
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:Student_id]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find_by(id: params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def destroy
     User.find(params[:id]).destroy
     redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name)
   end
 end
